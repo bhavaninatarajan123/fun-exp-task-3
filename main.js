dbd77de8af5f3b2b8bc8e809f99a52a73894e961
priyankacute <bhavanimurugesh2000@gmail.com>
2023-10-20 19:13:06 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,13 @@
+let a=6
+console.log(a)
+let b=prompt("enter the number");
+let num=function(a,b){
+    if(a==b){
+        document.write("good work")
+    }
+
+    else{
+        document.write("not matched")
+    }
+}
+(num(a,b))
